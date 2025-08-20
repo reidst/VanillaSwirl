fdb8267ff79858d9edd2102d01ad991e39bce4a5
Katherine Reid <reidst@hendrix.edu>
2025-08-20 13:11:18 -0500

    name all backups in a batch consistently

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-if scripts/are_servers_running; then
+if scripts/are_servers_running.sh; then
     echo "VanillaSwirl Error: cannot backup when servers are running."
     exit 1
 fi
 if [ -z "$(ls -A servers)" ]; then exit; fi
+backup_name="${1:-$(date +%Y-%m-%d_%H-%M-%S)}"
 for server in servers/*; do
     name=${server#*/}
     mkdir -p backups/$name
-    date=$(date +%Y-%m-%d_%H-%M-%S)
-    tar -czf backups/$name/$date.tar.gz -C $server .
+    tar -czf backups/$name/$backup_name.tar.gz -C $server .
     find backups/$name -type f -mtime +14 -delete
 done
