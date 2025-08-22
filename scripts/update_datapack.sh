a692b0953a5fc3bcc5386df73a7d4f8382d28b3f
Katherine Reid <reidst@hendrix.edu>
2025-08-21 22:53:39 -0500

    extract datapack generation logic

---
+++ scripts/update_datapack.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+function snake_to_title_case {
+	local words
+	IFS='_' read -a words <<< "$1"
+	local len="${#words[@]}"
+	for (( i=0; i<len; i++ )); do
+		words[$i]=${words[$i]^}
+	done
+	local capitalized=$(join_by '_' "${words[@]}")
+	printf '%s' "${capitalized//_/\ }"
+}
+
+if ! ls servers/*/ >/dev/null 2>&1; then
+    echo "VanillaSwirl Error: there are no servers to update."
+    exit 1
+fi
+if [ ! -f hostname.txt ] || [ -z "$(cat hostname.txt)" ]; then
+    echo "VanillaSwirl Error: missing or empty hostname.txt file."
+    exit 1
+fi
+actions=''
+for server in servers/*/; do
+    server=${server%/}
+    server_name=${server#*/}
+    server_display_name="$(snake_to_title_case $server_name)"
+    port="$(grep -sh '^server-port=' $server/server.properties | tail -1 | cut -d'=' -f 2)"
+    if [ -z $port ]; then
+        echo "VanillaSwirl Error: unable to update datapacks because the server $server_name has no port."
+        exit 1
+    fi
+    if [ -n $actions ]; then
+        actions+=','
+    fi
+    actions+="{\"label\":\"Warp to ${server_display_name}\",\"action\":{\"type\":\"run_command\",\"command\":\"trigger vanillaswirl.warp set $port\"}}"
+done
+for server in servers/*/; do
+    world_name="$(grep -sh '^level-name=' $server/server.properties | tail -1 | cut -d'=' -f 2)"
+    world_name=${world_name:-world}
+    datapack_path=$server/$world_name/datapacks/vanillaswirl
+    mkdir -p $datapack_path
+    rm -r $datapack_path 2>/dev/null
+    cp -r datapack/ $datapack_path
+    sed -i 's/\[\]/\['"$actions"'\]/' $datapack_path/data/vanillaswirl/dialog/warp_menu.json
+    sed -i "s/localhost/$(cat hostname.txt)/" $datapack_path/data/vanillaswirl/function/transfer.mcfunction
+done
+if screen -ls | grep -q '\.vanillaswirl\.'; then
+    scripts/global_command.sh reload
+fi
